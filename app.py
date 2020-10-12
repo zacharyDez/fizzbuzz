497ec79e1e342dd75feb781f8d7a544a2c3738e7
dezz2201 <zachary.deziel@usherbrooke.ca>
2020-10-12 14:14:05 -0400

    got flask monitoring dashboard working on local windows machine

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, request
+import flask_monitoringdashboard as dashboard
 
 from fizzbuzz.fizzbuzz import main
 
+# useful link
+# https://medium.com/flask-monitoringdashboard-turtorial/monitor-your-flask-web-application-automatically-with-flask-monitoring-dashboard-d8990676ce83
+
 app = Flask(__name__)
+
+dashboard.config.init_from(file='./dashboard.cfg')
+dashboard.bind(app)
 
 
 @app.route('/')
